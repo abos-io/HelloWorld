a315b9dda28d6b67cd2af1f6cc73456e1f38cbfd
Aaron Bos <aaron.b.bos@gmail.com>
2017-06-08 20:52:08 -0700

    to remove previous changes and rebuild program

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,14 +7,6 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
-
-            test extra stuff.
-                but now it is different. 
-
-
-                FORKED YO. why is this not saving as the forked version?
-
         }
     }
 }
